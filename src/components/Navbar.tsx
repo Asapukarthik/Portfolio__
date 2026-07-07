4f75e0f2f899d966fd82cf6888768f3f0ec6e70f
Asapukarthik <karthikasapu21@gmail.com>
2026-07-07 16:49:54 +0530

    change the resume

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -149,7 +149,7 @@ function Navbar() {
               ))}
               <li className="mt-4">
                 <a
-                  href="/karthik_asapu_resume.pdf"
+                  href="/karthik_resume.pdf"
                   target="_blank"
                   rel="noopener noreferrer"
                   className="flex w-full items-center justify-center gap-2 rounded-2xl bg-white px-4 py-4 text-sm font-bold text-black"
